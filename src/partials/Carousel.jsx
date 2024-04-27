1de04a67a862d5febe5dfeff82bf0b739af2388a
Akshat <daiyaakshat4@gmail.com>
2024-04-27 17:45:20 +0530

    improved carousel...

--- src/partials/Carousel.jsx
+++ src/partials/Carousel.jsx
@@ -1,43 +1,37 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react'
 import { MdLightbulbOutline, MdOutlineReplay } from "react-icons/md";
 import { HiOutlineSpeakerWave } from "react-icons/hi2";
 import { RiFullscreenLine } from "react-icons/ri";
 import { FaAngleRight, FaAngleLeft } from "react-icons/fa6";
 
 function Carousel() {
+    const buttons = ['Study', 'Quiz', 'Test', 'Game', 'Others']
+    const [activeButton, setActiveButton] = useState(0)
+
+    const handleButtonClick = (buttonIndex) => {
+        setActiveButton(buttonIndex);
+    }
+
     return (
         <div className="carousel py-7">
             <h2 className='forColor md:text-4xl sm:text-2xl text-lg font-medium pb-7'>Relations and Functions ( Mathematics )</h2>
 
+            {/*  */}
+
             <div className="carouselFile my-7">
                 <ul className="flex items-center justify-center md:gap-10 gap-6">
-                    <li>
-                        <Link to='#' className='text-[#06286E] text-lg font-semibold py-2 border-b-2 border-[#06286E]'>
-                            Study
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to='#' className='text-[#696671]'>
-                            Quiz
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to='#' className='text-[#696671]'>
-                            Test
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to='#' className='text-[#696671]'>
-                            Game
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to='#' className='text-[#696671]'>
-                            Others
-                        </Link>
-                    </li>
+                    {buttons.map((button, i) => (
+                        <li key={i}>
+                            <button
+                                className={`${activeButton === i ? 'text-[#06286E] text-lg font-semibold py-2 border-b-2 border-[#06286E]' : 'text-[#696671]'}`}
+                                onClick={() => handleButtonClick(i)}
+                            >
+                                {button}
+                            </button>
+                        </li>
+                    ))}
                 </ul>
+
 
                 <div className="carouselImg flex items-center justify-center flex-col m-7">
                     <div className="img flex items-center justify-center w-[287px] sm:w-[500px] md:w-[712px] h-[393.19px] rounded-[42.51px] relative">
